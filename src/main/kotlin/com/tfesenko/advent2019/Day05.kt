2eaba5691c1fbc1d20165647dbb6d6ccbd87af73
Tatiana Fesenko <tatiana.fesenko@gmail.com>
2019-12-05 12:20:31 -0500

    Day 5: Sunny with a Chance of Asteroids. Part 2

--- src/main/kotlin/com/tfesenko/advent2019/Day05.kt
+++ src/main/kotlin/com/tfesenko/advent2019/Day05.kt
@@ -2,9 +2,13 @@ package com.tfesenko.advent2019
 
 import java.lang.IllegalArgumentException
 
+/**
+ * Solution for Day 5: Sunny with a Chance of Asteroids
+ * https://adventofcode.com/2019/day/5
+ */
 fun main() {
     Day05(Resources.getResource("Day05.txt").readText().trim())
-        .solve1()
+        .solve2()
 }
 
 class Day05(private val rawInput: String) {
@@ -13,19 +17,23 @@ class Day05(private val rawInput: String) {
             .map { it.toInt() }
             .toIntArray()
 
-    fun solve1(): Int = Program(values).run()
+    fun solve1(): Int = Program(values).run() // use 1 for input
 
+    fun solve2(): Int = Program(values).run() // use 5 for input
 
     internal class Program(private val originalMemory: IntArray) {
         private val memory = originalMemory.copyOf()
         private var instructionPointer: Int = 0
+        private var startOfCurrentInstruction: Int = instructionPointer
 
         fun run(): Int {
-            while (executeInstruction()) { }
+            while (executeInstruction()) {
+            }
             return memory[0]
         }
 
         private fun executeInstruction(): Boolean {
+            startOfCurrentInstruction = instructionPointer
             val instruction = nextValue()
             val code = instruction % 100
             val modes = instruction / 100
@@ -34,18 +42,40 @@ class Day05(private val rawInput: String) {
                 2 -> multiply(modes)
                 3 -> input(modes)
                 4 -> output(modes)
+                5 -> jumpIfTrue(modes)
+                6 -> jumpIfFalse(modes)
+                7 -> lessThan(modes)
+                8 -> equals(modes)
                 99 -> halt(modes)
-                else -> throw IllegalArgumentException("Illegal command at index $instructionPointer: ${memory[instructionPointer]}")
+                else -> throw IllegalArgumentException("Illegal instruction at index $instructionPointer: $instruction")
             }
         }
 
-        private fun halt(mode: Int) = false
+        private fun add(mode: Int): Boolean {
+            var modes = mode
+            val param1 = resolveParameter(nextValue(), modes % 10)
+            modes /= 10
+            val param2 = resolveParameter(nextValue(), modes % 10)
+            val target = nextValue()
+            writeTo(target, param1 + param2)
+            return true
+        }
+
+        private fun multiply(mode: Int): Boolean {
+            var modes = mode
+            val param1 = resolveParameter(nextValue(), modes % 10)
+            modes /= 10
+            val param2 = resolveParameter(nextValue(), modes % 10)
+            val target = nextValue()
+            writeTo(target, param1 * param2)
+            return true
+        }
 
         private fun input(mode: Int): Boolean {
             val position = nextValue()
             print("Please enter value: ")
             val input = readLine()?.toInt()!!
-            memory[position] = input
+            writeTo(position, input)
             return true
         }
 
@@ -55,35 +85,66 @@ class Day05(private val rawInput: String) {
             return true
         }
 
-        private fun add(mode: Int): Boolean {
+        private fun jumpIfTrue(mode: Int): Boolean {
             var modes = mode
-            val param1 = resolveParameter(nextValue(), modes%10)
+            val param1 = resolveParameter(nextValue(), modes % 10)
             modes /= 10
-            val param2 = resolveParameter(nextValue(), modes%10)
-            modes /= 10
-            val target = nextValue()
-            memory[target] = param1 + param2
+            val param2 = resolveParameter(nextValue(), modes % 10)
+            if (param1 != 0) {
+                instructionPointer = param2
+            }
             return true
         }
 
-        private fun multiply(mode: Int): Boolean {
+        private fun jumpIfFalse(mode: Int): Boolean {
             var modes = mode
-            val param1 = resolveParameter(nextValue(), modes%10)
+            val param1 = resolveParameter(nextValue(), modes % 10)
             modes /= 10
-            val param2 = resolveParameter(nextValue(), modes%10)
+            val param2 = resolveParameter(nextValue(), modes % 10)
             modes /= 10
-            val target = nextValue()
-            memory[target] = param1 * param2
+            if (param1 == 0) {
+                instructionPointer = param2
+            }
             return true
+        }
+
+        private fun lessThan(mode: Int): Boolean {
+            var modes = mode
+            val param1 = resolveParameter(nextValue(), modes % 10)
+            modes /= 10
+            val param2 = resolveParameter(nextValue(), modes % 10)
+            modes /= 10
+            val param3 = nextValue()
+            writeTo(param3, if (param1 < param2) 1 else 0)
+            return true
+        }
+
+        private fun equals(mode: Int): Boolean {
+            var modes = mode
+            val param1 = resolveParameter(nextValue(), modes % 10)
+            modes /= 10
+            val param2 = resolveParameter(nextValue(), modes % 10)
+            modes /= 10
+            val param3 = nextValue()
+            writeTo(param3, if (param1 == param2) 1 else 0)
+            return true
+        }
+
+        private fun halt(mode: Int) = false
+
+        private fun writeTo(index: Int, value: Int) {
+            memory[index] = value
+            // restore pointer
+            if (index == startOfCurrentInstruction) instructionPointer = startOfCurrentInstruction
         }
 
         private fun nextValue(): Int {
-            val value  = memory[instructionPointer]
+            val value = memory[instructionPointer]
             instructionPointer++
             return value
         }
 
-        private fun resolveParameter(rawValue: Int, mode: Int = 0):Int {
+        private fun resolveParameter(rawValue: Int, mode: Int = 0): Int {
             return if (mode == 0) memory[rawValue] else rawValue
         }
     }
